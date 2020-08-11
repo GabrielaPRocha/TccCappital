ec9e4ef58b87a97199f38500939daec6f1eed87f
Gabriela <gabriela.rocha71@etecia.com.br>
2020-08-11 20:58:07 -0300

    Head

---
+++ Cappital.php
@@ -0,0 +1,24 @@
+<?php include "cabecalho.php"?>
+<body>
+    <nav id="menu" class="nav-extended">
+        <div class="nav-wrapper">
+            <a class="brand-logo center">
+                <h1>Cappital</h1>
+                <ul id="nav-mobile" class="right ">
+                </ul>
+        </div>
+
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent">
+                <li class="tab"><a class="active" href="#test1">Pagina inicial </a></li>
+                <li class="tab"><a class="active" href="#test2">Contato</a></li>
+                <li class="tab"><a class="active" href="#test3">Quem somos</a></li>
+            </ul>
+        </div>
+    </nav>
+    
+    
+   
+</body>
+
+</html>
